3fe81e7c77b5f5e03675fe1df24b98b1a26edd2b
Vit0rrr <vitorhmeira@gmail.com>
2024-04-19 17:09:35 -0300

    EXE-8

--- Main.java
+++ Main.java
@@ -1,16 +1,21 @@
-import java.io.IOException;
 import java.util.Scanner;
+import java.util.Locale;
 
 public class Main {
  
-    public static void main(String[] args) throws IOException {
-    Scanner sc =  new Scanner(System.in);
-   
-     double raio = sc.nextDouble();
-	 double n = 3.14159;
-	 double area = n * Math.pow(raio,2);
-    	    
-     System.out.printf("A=%.4f",area);
-     
+    public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+    Locale.setDefault (Locale.US);
+    
+    int number = sc.nextInt();
+    int HorasTrabalhadas = sc.nextInt();
+    Double valorHora = sc.nextDouble();
+    
+    Double salario = valorHora * HorasTrabalhadas ;
+    
+    System.out.printf("NUMBER = %d\n",number);
+    System.out.printf("SALARY = U$ %.2f%n",salario);
+
+       
     }
-}
+ 
