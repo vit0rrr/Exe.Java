1a9e5363fa870b1ba5583f08f003be3734a5230c
Vit0rrr <vitorhmeira@gmail.com>
2024-04-30 14:57:03 -0300

    exe-impar.java

---
+++ NumImpar2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+ 
+
+public class Main {
+ 
+    public static void main(String[] args)  {
+    Scanner sc = new Scanner(System.in);
+        
+      int maiorValor = 0;
+      int posicaoMaiorValor= 0;
+
+        
+       for (int i = 1; i <= 3; i++){
+           
+           int valor = sc.nextInt();
+           
+    	   if (valor > maiorValor) {
+    	       maiorValor = valor ;
+    	       posicaoMaiorValor = i;               
+           }
+       	}       
+       System.out.println(maiorValor);
+       System.out.println(posicaoMaiorValor);
+       sc.close();       
+  }        
+}
+ 
